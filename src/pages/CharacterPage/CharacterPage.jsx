cd313839aa7935f52716349f10c8284c9c0f1945
kdrenata <kd.renata09@gmail.com>
2025-03-17 19:00:54 +0100

    padination done

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -6,36 +6,66 @@ import axios from "axios";
 export const CharacterPage = () => {
 
     const [characters, setCharacters] = useState([])
-    // debugger
+
+    const [info, setInfo] = useState({
+        count: 0,
+        pages: 0,
+        next: null,
+        prev: null,
+    })
+    const fetchData = (url) => {
+        axios.get(url).then((res) => {
+            setCharacters(res.data.results);
+            setInfo(res.data.info);
+        })
+    }
 
     useEffect(() => {
-    // debugger
-        axios.get('https://rickandmortyapi.com/api/character').then((res) => {
-            // debugger
-            console.log(res.data);
-            setCharacters(res.data.results);
-        })
+        fetchData('https://rickandmortyapi.com/api/character')
     }, [])
 
-    // debugger
+
+    const previousPageHandler = () => {
+
+        fetchData(info.prev)
+    }
+
+    const nextPageHandler = () => {
+        fetchData(info.next)
+    }
+
     return (
         <div className={'pageContainer'}>
             <h1 className={'pageTitle'}>CharacterPage</h1>
             {characters.length && (
-                <div className={s.characters}>
+                <>
                     {
-                        characters.map( (character) => {
-                            // debugger
-                            return (
-                                <div key={character.id} className={s.character}>
-                                    <div className={s.characterLink}>{character.name}</div>
-                                    <img src={character.image} alt={`${character.name} avatar`}/>
-                                </div>
-                            )
-                        })
+                        <div className={s.characters}>
+                            {characters.map((character) => {
+                                return (
+                                    <div key={character.id} className={s.character}>
+                                        <div className={s.characterLink}>{character.name}</div>
+                                        <img src={character.image} alt={`${character.name} avatar`} />
+                                    </div>
+                                )
+                            })}
+                        </div>
                     }
-                </div>
-
+                    <div className={s.buttonContainer}>
+                        <button
+                            disabled={info.prev === null}
+                            className="linkButton"
+                            onClick={previousPageHandler}>
+                            Назад
+                        </button>
+                        <button
+                            disabled={info.next === null}
+                            className="linkButton"
+                            onClick={nextPageHandler}>
+                            Вперед
+                        </button>
+                    </div>
+                </>
             )}
         </div>
     )
