73283f7c6516f2c333672247ce1a61f5eb28a136
kdrenata <kd.renata09@gmail.com>
2025-03-18 12:30:46 +0100

    dynamic routing done

--- src/pages/CharacterPage/CharacterPage.jsx
+++ src/pages/CharacterPage/CharacterPage.jsx
@@ -2,22 +2,29 @@ import s from "./CharacterPage.module.css"
 
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Link} from "react-router";
 
 export const CharacterPage = () => {
 
     const [characters, setCharacters] = useState([])
-
+    const [error, setError] = useState(null)
     const [info, setInfo] = useState({
         count: 0,
         pages: 0,
         next: null,
         prev: null,
     })
+
     const fetchData = (url) => {
-        axios.get(url).then((res) => {
-            setCharacters(res.data.results);
-            setInfo(res.data.info);
-        })
+        axios.get(url)
+            .then((res) => {
+                setCharacters(res.data.results);
+                setInfo(res.data.info);
+                setError(null)
+            })
+            .catch( (error) => {
+                setError(error.response.data.error)
+            })
     }
 
     useEffect(() => {
@@ -33,18 +40,25 @@ export const CharacterPage = () => {
     const nextPageHandler = () => {
         fetchData(info.next)
     }
+    const searchHandler = (event) => {
+        const value = event.currentTarget.value
+        fetchData(`https://rickandmortyapi.com/api/character?name=${value}`)
+    }
 
     return (
         <div className={'pageContainer'}>
             <h1 className={'pageTitle'}>CharacterPage</h1>
-            {characters.length && (
+            <input type="search" className={s.search} onChange={searchHandler} placeholder="Search..." />
+            {error && <div className="errorMessage">😥{error }</div>}
+
+            {!error && characters.length && (
                 <>
                     {
                         <div className={s.characters}>
                             {characters.map((character) => {
                                 return (
                                     <div key={character.id} className={s.character}>
-                                        <div className={s.characterLink}>{character.name}</div>
+                                        <Link to={`/characters/${character.id}`} className={s.characterLink}>{character.name}</Link>
                                         <img src={character.image} alt={`${character.name} avatar`} />
                                     </div>
                                 )
